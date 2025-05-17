b10415b865ec3936db2c81a95e4e1997cf985cd5
Shmuel Mekonen <shmuelme@checkpoint.com>
2025-05-18 02:05:22 +0300

    Fix

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,12 +1,12 @@
-import express from 'express';
-import { login, logout, signup, updateProfile, checkAuth} from '../controllers/auth.controller.js';
-import {protectRoute} from '../middleware/auth.middleware.js'
+import express from "express";
+import { checkAuth, login, logout, signup, updateProfile } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.post('/signup', signup);
-router.post('/login', login);
-router.post('/logout', logout);
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
 
 router.put("/update-profile", protectRoute, updateProfile);
 
